7077737413ea5d71ed939b793cad230425858f37
Erick Paepke <epaepke@gmail.com>
2016-01-27 11:06:10 -0800

    Add Bee class

--- src/Grub.js
+++ src/Grub.js
@@ -1,4 +1,13 @@
 class Grub {
+  constructor(age, color, food) {
+    this.age = age;
+    this.color = color;
+    this.food = food;
+  }
+
+  eat () {
+    return 'Mmmmmmmmm jelly';
+  }
 };
 
 export default Grub;
